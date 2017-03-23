31435837b461a3c85e2c5d97ccf688f7d2d33a63
Vlad Pistun <v.pistun@1plus1.tv>
2017-03-23 14:38:45 +0200

    Added flashmessages

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Navbar from './partials/navbar';
+import FlashMessagesList from './flash/flashMessagesList'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <FlashMessagesList />
         {this.props.children}
       </div>
     );
